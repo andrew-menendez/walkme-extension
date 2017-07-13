bc1574466fd30412c1bd8071cc3f516bd66b7fd9
Andrew Menendez <andrew.m@andrew-mendendez.local>
2017-07-13 14:44:31 -0700

    working on https

---
+++ inject.js
@@ -0,0 +1,21 @@
+function wm(_tab) {
+
+
+        chrome.tabs.executeScript(_tab.id, { file: "/scripts/walkme.js" }, function() {
+            // console.log('executed walkme.js');
+            alert('executed walkme.js')
+        });
+
+}
+
+
+
+///inject here
+console.log("I'm alive");
+//should trigger when the icon is clicked
+chrome.browserAction.onClicked.addListener(function(tab) {
+	//self executing function
+	wm(tab);
+	
+	
+});
